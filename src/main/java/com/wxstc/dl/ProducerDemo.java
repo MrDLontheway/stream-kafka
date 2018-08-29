9d8ed7592dab776bff64c222f3b2585abf796d5c
Administrator <726575153@qq.com>
2018-08-30 00:45:22 +0800

    1

---
+++ src/main/java/com/wxstc/dl/ProducerDemo.java
@@ -0,0 +1,85 @@
+package com.wxstc.dl;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class ProducerDemo implements Runnable{
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+
+    public ProducerDemo(String topicName) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "AnserInsight01:9092,AnserInsight02:9092,AnserInsight03:9092");//连接kafka集群 broker 列表
+        /**
+         * 此配置是 Producer 在确认一个请求发送完成之前需要收到的反馈信息的数量。 这个参数是为了保证发送请求的可靠性。以下配置方式是允许的：
+         acks=0 如果设置为0，则 producer 不会等待服务器的反馈。该消息会被立刻添加到 socket buffer 中并认为已经发送完成。在这种情况下，服务器是否收到请求是没法保证的，并且参数retries也不会生效（因为客户端无法获得失败信息）。每个记录返回的 offset 总是被设置为-1。
+         acks=1 如果设置为1，leader节点会将记录写入本地日志，并且在所有 follower 节点反馈之前就先确认成功。在这种情况下，如果 leader 节点在接收记录之后，并且在 follower 节点复制数据完成之前产生错误，则这条记录会丢失。
+         acks=all 如果设置为all，这就意味着 leader 节点会等待所有同步中的副本确认之后再确认这条记录是否发送完成。只要至少有一个同步副本存在，记录就不会丢失。这种方式是对请求传递的最有效保证。acks=-1与acks=all是等效的。
+         all 与 -1 等效的
+         */
+        props.put("acks", "all");
+        /**
+         * 若设置大于0的值，则客户端会将发送失败的记录重新发送，尽管这些记录有可能是暂时性的错误。请注意，这种 retry 与客户端收到错误信息之后重新发送记录并无区别。允许 retries 并且没有设置max.in.flight.requests.per.connection 为1时，记录的顺序可能会被改变。比如：当两个批次都被发送到同一个 partition ，第一个批次发生错误并发生 retries 而第二个批次已经成功，则第二个批次的记录就会先于第一个批次出现。
+         */
+        props.put("retries", 0);
+        /**
+         * 当将多个记录被发送到同一个分区时， Producer 将尝试将记录组合到更少的请求中。这有助于提升客户端和服务器端的性能。这个配置控制一个批次的默认大小（以字节为单位）。
+         当记录的大小超过了配置的字节数， Producer 将不再尝试往批次增加记录。
+
+         发送到 broker 的请求会包含多个批次的数据，每个批次对应一个 partition 的可用数据
+
+         小的 batch.size 将减少批处理，并且可能会降低吞吐量(如果 batch.size = 0的话将完全禁用批处理)。 很大的 batch.size 可能造成内存浪费，因为我们一般会在 batch.size 的基础上分配一部分缓存以应付额外的记录。
+         */
+        props.put("batch.size", 16384);
+        /**
+         * producer 会将两个请求发送时间间隔内到达的记录合并到一个单独的批处理请求中。通常只有当记录到达的速度超过了发送的速度时才会出现这种情况。然而，在某些场景下，即使处于可接受的负载下，客户端也希望能减少请求的数量。这个设置是通过添加少量的人为延迟来实现的&mdash；即，与其立即发送记录， producer 将等待给定的延迟时间，以便将在等待过程中到达的其他记录能合并到本批次的处理中。这可以认为是与 TCP 中的 Nagle 算法类似。这个设置为批处理的延迟提供了上限:一旦我们接受到记录超过了分区的 batch.size ，Producer 会忽略这个参数，立刻发送数据。但是如果累积的字节数少于 batch.size ，那么我们将在指定的时间内“逗留”(linger)，以等待更多的记录出现。这个设置默认为0(即没有延迟)。例如：如果设置linger.ms=5 ，则发送的请求会减少并降低部分负载，但同时会增加5毫秒的延迟。
+         * 增加请求延迟 降低负载
+         */
+        props.put("linger.ms", 1);
+        /**
+         * Producer 用来缓冲等待被发送到服务器的记录的总字节数。如果记录发送的速度比发送到服务器的速度快， Producer 就会阻塞，如果阻塞的时间超过 max.block.ms 配置的时长，则会抛出一个异常。
+         这个配置与 Producer 的可用总内存有一定的对应关系，但并不是完全等价的关系，因为 Producer 的可用内存并不是全部都用来缓存。一些额外的内存可能会用于压缩(如果启用了压缩)，以及维护正在运行的请求。
+         */
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        this.producer = new KafkaProducer<String, String>(props);
+        this.topic = topicName;
+    }
+
+    public void run() {
+        int messageNo = 1;
+        try {
+            for(;;) {
+                String messageStr="你好，这是第"+messageNo+"条数据";
+                System.out.println("成功发送了"+messageNo+"条");
+                producer.send(new ProducerRecord<String, String>(topic, "Message", messageStr));
+                //生产了10条就打印
+                if(messageNo%100==0){
+                    //System.out.println("发送的信息:" + messageStr);
+                }
+                //生产100条就退出
+                if(messageNo%100==0){
+                    System.out.println("成功发送了"+messageNo+"条");
+                    break;
+                }
+                messageNo++;
+//				Utils.sleep(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        ProducerDemo pd = new ProducerDemo("tstmessage");
+        Thread t1 = new Thread(pd);
+        t1.start();
+    }
+}
--- src/main/scala/com/wxstc/dl/stream/StreamingWordCount.scala
+++ src/main/scala/com/wxstc/dl/stream/StreamingWordCount.scala
@@ -2,15 +2,23 @@ package com.wxstc.dl.stream
 
 //import com.wxstc.util.LoggerLevels
 import kafka.serializer.StringDecoder
+import org.I0Itec.zkclient.ZkClient
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.streaming.kafka.KafkaManager
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 object StreamingWordCount {
   val updateFunc = (iterator: Iterator[(String, Seq[Int], Option[Int])]) => {
     iterator.flatMap{case(x,y,z)=> Some(y.sum + z.getOrElse(0)).map(n=>(x, n))}
   }
+
   def main(args: Array[String]): Unit = {
 //    LoggerLevels.setStreamingLogLevels(Level.WARN)
     val conf = new SparkConf().setAppName("StreamingWordCount").setMaster("local[2]")
@@ -31,25 +39,66 @@ object StreamingWordCount {
 
     Logger.getLogger("org").setLevel(Level.WARN)
 
-    val Array(brokers, topics, groupId) = args
-    val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String](
-      "metadata.broker.list" -> brokers,
-      "group.id" -> groupId,
-      "auto.offset.reset" -> "smallest"
-      //,"zookeeper.connect" -> ""
+    val Array(brokers, topics, groupId,zkQuorum) = args
+    val topicsSet = topics.split(",")
+    //    val kafkaParams = Map[String, String](
+    //      "metadata.broker.list" -> brokers,
+    //      "group.id" -> groupId,
+    //      "auto.offset.reset" -> "smallest"
+    //      //,"zookeeper.connect" -> ""
+    //    )
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> brokers,//"115.159.93.95:9092,115.159.78.168:9092,115.159.222.161:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> groupId,//"groupA",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
     )
-    val km = new KafkaManager(kafkaParams)
-    ssc.checkpoint("D:\\_vm\\ck")
+
+    /*val messages = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topicsSet, kafkaParams)
+    )
+    val ds1 = messages.map(record => (record.key, record.value))*/
+    //val km = new KafkaManager(kafkaParams)
+    ssc.checkpoint("E:\\_vm\\ck")
+
+    var kafkaStream: InputDStream[ConsumerRecord[String, String]] = null
     //socket 接收数据 127.0.0.1
+    val zk = new ZkClient(zkQuorum)
+    var fromOffsets: Map[TopicPartition, Long] = Map() //kafka 偏移量管理
 
-    val messages = km.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
+    val stream = if (zk.znodeIsExists(s"${topic}offset")) {
+      val nor = zk.znodeDataGet(s"${topic}offset")
+      val newOffset = Map(new TopicPartition(nor(0).toString, nor(1).toInt) -> nor(2).toLong)//创建以topic，分区为k 偏移度为v的map
+      println(s"[ DealFlowBills2 ] --------------------------------------------------------------------")
+      println(s"[ DealFlowBills2 ] topic ${nor(0).toString}")
+      println(s"[ DealFlowBills2 ] Partition ${nor(1).toInt}")
+      println(s"[ DealFlowBills2 ] offset ${nor(2).toLong}")
+      println(s"[ DealFlowBills2 ] zk中取出来的kafka偏移量★★★ $newOffset")
+      println(s"[ DealFlowBills2 ] --------------------------------------------------------------------")
+      KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams, newOffset))
+    } else {
+      println(s"[ DealFlowBills2 ] --------------------------------------------------------------------")
+      println(s"[ DealFlowBills2 ] 第一次计算,没有zk偏移量文件")
+      println(s"[ DealFlowBills2 ] 手动创建一个偏移量文件 ${topic}offset 默认从0号分区 0偏移度开始计算")
+      println(s"[ DealFlowBills2 ] --------------------------------------------------------------------")
+      zk.znodeCreate(s"${topic}offset", s"$topic,0,0")
+      val nor = zk.znodeDataGet(s"${topic}offset")
+      val newOffset = Map(new TopicPartition(nor(0).toString, nor(1).toInt) -> nor(2).toLong)
+      KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams, newOffset))
+    }
 
-//    val Array(zkQuorum, group, topics, numThreads) = args
+    //    val Array(zkQuorum, group, topics, numThreads) = args
 //    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
 //    val messages = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap, StorageLevel.MEMORY_AND_DISK_SER)
-    messages.print()
+    ds1.foreachRDD(rdd=>{
+      println(rdd.count())
+    })
+    ds1.print(5)
     ssc.start()
     ssc.awaitTermination()
   }
